11ee16feaf0f80f52ebdb028eaa8d3baf277a66c
AnetkaV <52952225+AnetkaV@users.noreply.github.com>
2020-10-10 21:47:44 +0200

    starting
    my starting python, just try, nothing important, etc.

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+print ('Hello world')
+
+print("which one")
+print("gonna work?")
+#there are two different Python verisons. Both python 2 and 3 are used throught the globe. 
+#The most significant difffernce between the two is how you write a "print"statemnet.
+#In python 3 has parentheses.
+
+print ("This is a good string")
+print ('You can use single quotes or double quotes for a string')
+
+print ("this is ") + ("a good string")
+
+#without or miss spelled quotes will popup error
+
+#VARIABELS
+

4ecf5ac538b9f9fd790a4fb125e6e187cfde2200
AnetkaV <52952225+AnetkaV@users.noreply.github.com>
2020-11-22 13:29:39 +0100

    add
    adding files i talked about in README

---
+++ Python/smth.py
@@ -0,0 +1,34 @@
+first_name = "Aneta"
+second_name = " Vankova"
+
+print(first_name + second_name )
+
+
+active_users = 5
+number_of_applications = 6 + 1 
+
+print(active_users )
+print(number_of_applications)
+
+
+status = "Watching Netflix"
+status = "Relaxing at the beach"
+print(status)
+
+
+default_option = "upload"
+new_status = "download"
+
+new_status = default_optionm
+print(new_status)
+
+powered_on = True 
+
+correct = True
+not_correct = False
+print(correct)
+print(not_correct)
+
+print(not True)
+print(not False)
+
